49c5cf7db57cafbe15a23f3ff9b0a60ebd7e56e5
nareshpenchikala <70266217+nareshpenchikala@users.noreply.github.com>
2020-09-16 12:05:01 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import find_packages
+from setuptools import setup
+
+REQUIRED_PACKAGES = [
+  'scikit-learn==0.20.1',
+]
+
+setup(
+    name='trainer',
+    version='0.1',
+    install_requires=REQUIRED_PACKAGES,
+    packages=find_packages(),
+    include_package_data=True,
+    requires=[]
+)
